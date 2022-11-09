8af5cf06b3ca95fe9c6f626d9edab30c794f30a6
FranciscoTerron <franciscoterrondg@hotmail.com>
2022-11-09 10:52:15 -0300

    Tercer Preentrega

---
+++ js/index.js
@@ -0,0 +1,90 @@
+let contenedorProductos = document.getElementById("productosContenedor");
+
+
+// Creo el carrito
+
+let carrito = [];
+
+// Tomo los ids de los elementos en html para
+const contenedorCarrito = document.getElementById('carrito-contenedor');
+const contadorCarrito = document.getElementById('contadorCarrito');
+const precioTotal = document.getElementById('precioTotal');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('carrito')) {
+        carrito = JSON.parse(localStorage.getItem('carrito'));
+        actualizarCarrito();
+    }
+})
+
+// Utilizo el boton para vaciar el carrito y igualo la cantidad que tiene a 0 
+const botonVaciar = document.getElementById('vaciar-carrito');
+// Cuando presionan este boton la longitud del carrito se pone en 0
+botonVaciar.addEventListener('click', () => {
+    carrito.length = 0;
+    actualizarCarrito();
+})
+
+
+// Muestro dinamicamente los productos
+productos.forEach(producto => {
+    const div = document.createElement('div');
+    div.className = ('producto');
+    div.innerHTML = `
+    <img src="${producto.imagen}" alt = "">
+    <h5> ${producto.nombre} </h5>
+    <p> $ ${producto.precio} </p>
+    <button id="agregar${producto.id}"  "class="boton-agregar"> Agregar al carrito <i class="fa-solid fa-cart-shopping"></i></button>
+    `
+    contenedorProductos.append(div);
+    const boton = document.getElementById(`agregar${producto.id}`);
+    boton.addEventListener('click', () => {
+        agregarAlCarrito(producto.id);
+    })
+});
+
+// Funciones para agregar, eliminar y actualizar los elementos
+
+const agregarAlCarrito = (productoId) => {
+    // Quiero armar un condicional para que cuando se quede sin stock un producto le aparezca en la tarjeta
+    // que ese producto no tiene stock
+
+    const item = productos.find((producto) => producto.id === productoId);
+    carrito.push(item);
+    item.cantidad--;
+    actualizarCarrito();
+}
+
+
+
+
+const eliminarDelCarrito = (productoId) => {
+    const item = carrito.find((producto) => producto.id === productoId);
+    // Busco el elemento y luego me devuelve su item
+    const indice = carrito.indexOf(item);
+    // Aca lo borro
+    carrito.splice(indice, 1);
+    actualizarCarrito();
+}
+
+const actualizarCarrito = () => {
+    // Esta linea la uso para eliminar el nodo asi se va actualizando con la info nueva
+    contenedorCarrito.innerHTML = "";
+    carrito.forEach((producto) => {
+        const div = document.createElement('div');
+        div.className = ('productoEnCarrito');
+        div.innerHTML += `
+            <p> ${producto.nombre} </p>
+            <p> Precio : ${producto.precio} </p>
+            <p> Cantidad Disponible : ${producto.cantidad}</p>
+            <button onclick = "eliminarDelCarrito(${producto.id})" class="boton-eliminar"> <i class="fas fa-trash-alt"></i></button>
+        `
+
+        contenedorCarrito.append(div);
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    })
+    contadorCarrito.innerText = carrito.length;
+    // Utilizo el innerText para ir mostrando el valor del total de mi carrito
+    precioTotal.innerText = carrito.reduce((acc, producto) => acc + producto.precio, 0);
+}
---
+++ js/productos.js
@@ -0,0 +1,11 @@
+let productos = [
+    { id: 1, nombre: "Set regalo", precio: 7500, cantidad: 10, oferta: true, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404342/matesGaimenses/productos/paginaPrincipal/set_ojpcu7.jpg" },
+    { id: 2, nombre: "BOMBILLON PICO CURVO ESPECIAL", precio: 7875, cantidad: 0, oferta: false, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404342/matesGaimenses/productos/paginaPrincipal/bombillas_xzxn3h.jpg" }, {
+        id: 3, nombre: "SET FULL MARADONEANO", precio: 15000, cantidad: 20, oferta: true, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404342/matesGaimenses/productos/paginaPrincipal/equipo4_eklcnr.jpg"
+    },
+    { id: 4, nombre: "Set Imperial perzonalizado", precio: 14000, cantidad: 5, oferta: false, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404341/matesGaimenses/productos/paginaPrincipal/mate4_uh1utf.jpg" },
+    { id: 5, nombre: "Set Uruguayo", precio: 6000, oferta: true, cantidad: 6, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404341/matesGaimenses/productos/paginaPrincipal/mate3_awpr3c.jpg" },
+    {
+        id: 6, nombre: "Mate perzonalizado", precio: 7000, cantidad: 1, oferta: false, imagen: "https://res.cloudinary.com/dm7uyzmxv/image/upload/v1667404341/matesGaimenses/productos/paginaPrincipal/mate1_yt81b5.jpg"
+    }
+];
