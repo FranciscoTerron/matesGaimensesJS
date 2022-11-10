80ddf3d6e42211ce41c7f439368b3e8a4d8eecac
FranciscoTerron <franciscoterrondg@hotmail.com>
2022-11-10 11:43:14 -0300

    Realice los ultimos cambios en el jquerys.js

--- js/jquerys.js
+++ js/jquerys.js
@@ -14,24 +14,18 @@ $(document).ready(function () {
         $(this).addClass('itemActivo');
 
         // OCULTANDO PRODUCTOS =========================
-        $('.producto-item').css('transform', 'scale(0)');
-        function hideProduct() {
-            $('.producto-item').hide();
-        } setTimeout(hideProduct, 400);
+
+        $('.producto-item').hide();
+
 
         // MOSTRANDO PRODUCTOS =========================
-        function showProduct() {
-            $('.producto-item[category="' + catProduct + '"]').show();
-            $('.producto-item[category="' + catProduct + '"]').css('transform', 'scale(1)');
-        } setTimeout(showProduct, 400);
+        $('.producto-item[category="' + catProduct + '"]').show();
     });
 
     // MOSTRANDO TODOS LOS PRODUCTOS =======================
 
     $('.category_item[category="all"]').click(function () {
-        function showAll() {
-            $('.producto-item').show();
-            $('.producto-item').css('transform', 'scale(1)');
-        } setTimeout(showAll, 400);
+        $('.producto-item').show();
+
     });
 });
